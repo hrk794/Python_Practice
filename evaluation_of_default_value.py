961d12d08eacdd3918fd467c901c1b54929814e5
hrk794 <hrk794@gmail.com>
2018-11-24 19:30:33 +0530

    Behaviour of default values, if they are mutable objects

---
+++ evaluation_of_default_value.py
@@ -0,0 +1,27 @@
+
+
+def f1(a, L=[]):
+    L.append(a)
+    return L
+# The default value is evaluated once when the funtcion is called for the first time, and the new value is passed to the subsequent calls of the function. 
+
+print(f1(1))
+print(f1(2))
+print(f1(3))
+
+
+
+
+def f2(a, L=None):
+    if L is None:
+        L = []
+    L.append(a)
+    return L
+
+# Here we want to preserve the default value of L between multiple calls of f2, each of which return a modified value of L.
+# The method used is to keep the default value equal to 'None', and then inside the function check if L is None, and then assign it an empty list. Yet to get to get a clear picture on this
+
+print(f2(1))
+print(f2(2))
+print(f2(3))
+
