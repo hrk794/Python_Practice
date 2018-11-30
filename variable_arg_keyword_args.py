eafe4688955283efe02de36dc8bbf43e35e61812
hrk794 <hrk794@gmail.com>
2018-11-30 17:35:55 +0530

    Adding invalid function calls with keyword arguments

--- variable_arg_keyword_args.py
+++ variable_arg_keyword_args.py
@@ -21,4 +21,7 @@ print()
 # following would be invalid calls:
 
 
-
+#parrot()                        # required argument missing
+#parrot(voltage=5.0, 'dead')     # non-keyword argument after a keyword argument
+#parrot(110, voltage=200)        # duplicate value for the same argument
+#parrot(actor='John Cleese')     # unknown keyword argument
