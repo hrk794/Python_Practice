536ba952797dac96d5b01e6fda14c43c5234e462
hrk794 <hrk794@gmail.com>
2018-12-05 16:39:42 +0530

    unpacking argument list for function call using the * operator

---
+++ unpack_arg_lists.py
@@ -0,0 +1,5 @@
+args = [3, 6]
+
+print(list(range(*args)))
+
+# The range function requires separate start and stop arguments. Calling the function with the * operator unpacks the arguments out of the list/tuple. 
