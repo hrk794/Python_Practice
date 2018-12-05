a3ead81ebeb3b7ca2935e57a9caa8db4a14de222
hrk794 <hrk794@gmail.com>
2018-12-05 23:38:17 +0530

    Some more list methods

--- list_methods.py
+++ list_methods.py
@@ -24,3 +24,69 @@ print(list)
 list[len(list):] = ['nine', 'ten', 'eleven']
 
 print(list)
+
+
+# The insert method
+
+list.insert(0, 'zero')
+
+print(list)
+
+# The remove method:    list.remove(x)
+
+# Remove the first item from the list whose value is equal to x. It raises a ValueError if there is no such item.
+
+list.remove('zero')
+
+print(list)
+
+
+# The pop method. Remove the item at the given position in the list, and return it. If no index is specified, a.pop() removes and returns the last item in the list.
+# The item position in the method is optional.
+
+list.pop(9)
+
+print(list)
+
+list.pop()
+
+print(list)
+
+# The clear method
+
+# Remove all items from the list. Equivalent to del a[:].
+
+list.clear()
+
+print(list)
+
+
+# The index method
+
+# list.index(x[, start[, end]])
+# Return zero-based index in the list of the first item whose value is equal to x. Raises a ValueError if there is no such item.
+
+list = ['one', 'two', 'three', 'four', 'five']
+
+print(list)
+
+print(list.index('three'))
+
+print(list.index('three', 1, 3))
+
+print(list)
+
+
+# The count method  list.count(x)
+# Return the number of times x appears in the list.
+
+
+list.append('three')
+
+print(list)
+
+print(list.count('three'))
+
+print(list.count('two'))
+
+
