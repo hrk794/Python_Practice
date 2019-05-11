771bbcd3ce31e6e83eaead8e238fd42eca6166aa
hrk794 <hrk794@gmail.com>
2019-05-11 09:58:20 +0530

    Text to speech

---
+++ Text_to_Speech/spch.py
@@ -0,0 +1,12 @@
+#!/home/hrk/bin/python
+from gtts import gTTS
+
+text = ""
+
+with open("tts.txt", "r") as file:
+	for line in file:
+		text = text + line
+
+speech = gTTS(text)
+speech.save("audio.mp3")
+
